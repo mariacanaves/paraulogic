f243685930cb2b1325ea64f117335d8dd375d948
mariacanaves <mariacanavea@alumnat.iesdamiahuguet.net>
2024-01-23 12:45:22 +0100

    Petites millores

--- Paraula.js
+++ Paraula.js
@@ -1,26 +1,41 @@
-
-const secreta='SWIFT'
-
-
+// la paraula secreta
+const secreta='MARIA'
 
 function enviar() {
     let paraula =document.getElementById('paraula').value
+    // alerta en cas de més lletres
     if (paraula.length != secreta.length) {
         window.alert('HA DE TENIR 5 LLETRES')
     }
-    else if(paraula.toUpperCase()==secreta){
 
-        window.alert('HO HAS ACONSEGUIT')
+    // cas de victoria
+    else if(paraula.toUpperCase()===secreta){
+        // alerta
+        window.alert('HO HAS ACONSEGUIT, la paraula secreta era '+ secreta)
+        let paraulabona="<div class='resposta'>"
+        // escriu paraula
+        for(let i=0;i<secreta.length;i++){
+paraulabona += "<div class='slot green'>"
+            paraulabona+=secreta.charAt(i)
+            paraulabona+="</div>"
+        }
+        paraulabona+="</div>"
+        document.getElementById('resposta').innerHTML +=  paraulabona + "<br>"
     }
+
+    // altres casos (no està be la paraula)
     else{
         let text='<div class="resposta">'
         for(let i=0;i<paraula.length;i++){
+            // si la lletra està ben colocada
             if(paraula.toUpperCase().charAt(i)==secreta.charAt(i)){
                 text += "<div class='slot green'>"
                 }
+            // si la paraula te la lletra
             else if (secreta.includes(paraula.toUpperCase().charAt(i))==true){
                 text += "<div class='slot yellow'>"
             }
+            // si la paraula no te la lletra
             else{
                 text += "<div class='slot'>"
             }
@@ -35,28 +50,3 @@ function enviar() {
     }
 
 }
-
-
-
-
-
-
-
-
-function Lletres(paraula) {
-    let x=0
-    let y =0
-    let z =0
-    let lletra= paraula.toLowerCase()
-    for (let i=0;i<paraula.length;i++){
-        if (lletra.charAt(i)=='M'){ x++
-        }
-        else if (lletra.charAt(i).includes('a')==true){y++
-        }
-        else {
-z++
-        }
-    }
-}
-
-// Lletres('Maria') //3
